50781835c9a02f52503cbe03bdf111bf34a1bfb2
fdfcosta-telecon <fabricio.costa@teleconsistemas.com.br>
2023-03-18 13:20:43 -0300

    Configurações das entidades para criação das tabelas

---
+++ CatalogAPI/CatalogAPI.Infra.Data/Configurations/Entities/ProductConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using CatalogAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CatalogAPI.Infra.Data.Configurations.Entities
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(p => p.Description)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(p => p.Price)
+                .HasPrecision(10, 2);
+
+            builder.Property(p => p.Stock)
+                .IsRequired();
+
+            builder.Property(p => p.UrlImage)
+                .IsRequired(false);
+
+
+            builder.HasOne(p => p.Category)
+                .WithMany(p => p.Products)
+                .HasForeignKey(p => p.CategoryId);
+        }
+    }
+}
